8006edc5eeb96c23831b3974a4349c1bc98e32c7
beast-devAI <enotest75@gmail.com>
2024-11-06 16:46:46 +0530

    Basic File Upload

---
+++ src/com/selenium/practice/BasePractice/BasicFileUpload.java
@@ -0,0 +1,21 @@
+package com.selenium.practice.BasePractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicFileUpload {
+
+    final static String appURL = "https://omayo.blogspot.com/";
+    static String loc_File_Upload = "form>input[type='file']";
+    final static String filePath="resources/SeleniumFileTestUpload.txt";
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get(appURL);
+        driver.manage().window().maximize();
+        driver.findElement(By.cssSelector(loc_File_Upload)).sendKeys(filePath);
+
+    }
+}
+
