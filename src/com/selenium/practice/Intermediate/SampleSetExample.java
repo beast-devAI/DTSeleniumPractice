92770ab7043c1c1dd82c04e23b904d782b112e94
beast-devAI <enotest75@gmail.com>
2024-11-10 15:52:24 +0530

    SampleSetExample

---
+++ src/com/selenium/practice/Intermediate/SampleSetExample.java
@@ -0,0 +1,61 @@
+package com.selenium.practice.Intermediate;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class SampleSetExample {
+
+    public static void main(String[] args) {
+
+        // Use of collection List
+
+/*
+        List<Object> lst = new ArrayList<Object>();
+
+        System.out.println(lst.size());
+
+        lst.add("Mango");
+        lst.add("Banana");
+        lst.add("Apple");
+        lst.add("Orange");
+        lst.add("Banana");
+        lst.add("Apple");
+        lst.add("Orange");
+
+
+         System.out.println(lst.size());
+         System.out.println(lst.get(4));
+
+        for (Object ob : lst) {
+
+            System.out.println(ob);
+
+        }
+
+*/
+
+        // Use of Set List
+
+        /*
+        Set<String> setDemo = new HashSet<>();
+
+        setDemo.add("Ankit");
+        setDemo.add("Vijay");
+        setDemo.add("Harendra");
+        setDemo.add("Ankit01");
+
+        System.out.println(setDemo.size());
+
+        for(String str:setDemo){
+
+            System.out.println(str);
+        }
+
+
+    }
+       */
+
+    }
+}
